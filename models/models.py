4e2643a227ef49fad211b4ac24ee0a3742cd0c5f
Pedro Gracia <lasarux@MacBook-de-Pedro.local>
2017-01-24 09:37:16 +0100

    * fixed a name relation mistake

--- models/models.py
+++ models/models.py
@@ -5,5 +5,5 @@ from openerp import models, fields, api
 class product_prodcut(models.Model):
     _inherit = 'product.product'
 
-    taxes_id = fields.Many2many('account.tax', 'product_prodcut_taxes_rel', 'prod_id', 'tax_id', string='Customer Taxes',
+    taxes_id = fields.Many2many('account.tax', 'product_product_taxes_rel', 'prod_id', 'tax_id', string='Customer Taxes',
         domain=[('type_tax_use', '=', 'sale')])
